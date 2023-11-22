5b7d16eb241cd8d1f75d8f4407ddc606104bfdc4
Bassim Durzi <bassimdurzi@gmail.com>
2023-11-22 22:22:24 +0100

    ex13

---
+++ ex13.js
@@ -0,0 +1,13 @@
+const person = {
+    name: "Adam Adamsson",
+    age: 47,
+    SSN: "19761125-1234",
+    isSmoke: false,
+    address:{
+        street: "StoraGatan 1",
+        city: "Strockholm",
+        postCode: "131 50"
+    }
+    }
+
+    console.log(person);
