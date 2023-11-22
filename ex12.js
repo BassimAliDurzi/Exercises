8396313cf24fbb72226175bbac15438a2296ff71
Bassim Durzi <bassimdurzi@gmail.com>
2023-11-22 22:13:36 +0100

    ex12

---
+++ ex12.js
@@ -0,0 +1,7 @@
+function squreNumbers(numbers) {
+  let squreNumber = numbers.map((x) => x * x);
+  console.log(squreNumber);
+}
+
+let numbers = [1, 2, 3, 4, 5, 6, 7, 8, 9, 10];
+squreNumbers(numbers);
